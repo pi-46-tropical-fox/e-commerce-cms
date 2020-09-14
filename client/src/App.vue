b676ed932b61813dea8d320002513f069a022262
Naufal Muhamad Siddiq <obostea12@gmail.com>
2020-09-14 15:49:20 +0700

    baru test aja belum beres juga

---
+++ client/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <Login></Login>
+        <Register></Register>
+        <Home></Home>        
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+import Login from "./views/Login"
+import Register from "./views/Register"
+import Home from "./views/Dashboard"
+export default {
+    data(){
+        page : 'login'
+        return {message: "E-Commerce CMS"}
+    },
+    components:{
+        Login,
+        Register,
+        Home,
+    },
+    methods:{
+        logout(){
+            this.page = 'login'
+            localStorage.clear()
+        },
+        changePage(page){
+            this.page = page
+            if(page === 'Dashboard'){
+                this.fetchData()
+            }
+        },
+        fetchData(){
+            axios({
+                method : 'GET',
+                url: 'http://localhost:3000/',
+                headers:{
+                    access_token : localStorage.access_token
+                }
+            })
+        },
+    },
+    created(){
+        if(localStorage.getItem(access_token)){
+            this.page = 'Dashboard'
+            this.fetchData()
+        }else{
+            this.page = 'Login'
+        }
+    }
+}   
+</script>
+
+<style lang="stylus">
+
+</style>
