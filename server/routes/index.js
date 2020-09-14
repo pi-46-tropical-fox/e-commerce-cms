8ef11fb8424be83cbb6c9cacc0fc5b47d019dca0
Fandi Ario <fandi.ario10@gmail.com>
2020-09-14 23:56:57 +0700

    Release 1 -Add User Test Register

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,5 @@ const UserController = require ("../controllers/UserController.js")
 router.post ("/register", UserController.register)
 // router.post ("/login", UserController.login)
 
+
 module.exports = router
