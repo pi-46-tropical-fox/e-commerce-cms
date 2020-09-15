452be2af5700ffdc33294a094852a5bfb2f59355
ArvianditoCaessara <arviandito.caessara@mail.com>
2020-09-16 00:15:00 +0700

    2nd commit: after installing & setup vue cli

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Card></Card>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Card from '../components/itemCard'
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  }
+}
+</script>
