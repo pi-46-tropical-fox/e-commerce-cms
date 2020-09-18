208adef608103ecde285831cc36915d55891c5bf
Eas <me@eas.web.id>
2020-09-19 04:29:13 +0700

    add auth

--- server/routes/endpoints/banners.js
+++ server/routes/endpoints/banners.js
@@ -1,11 +1,12 @@
 const bannerEndpoints = require('express').Router()
+const { authenticate, authorize } = require('../../middleware/auth')
 const { BannerController } = require('../../controllers')
 
 bannerEndpoints
 .get('/', BannerController.read)
 .get('/:id', BannerController.readOne)
-.post('/', BannerController.create)
-.put('/:id', BannerController.update)
-.delete('/:id', BannerController.delete)
+.post('/', authenticate, authorize, BannerController.create)
+.put('/:id', authenticate, authorize, BannerController.update)
+.delete('/:id', authenticate, authorize, BannerController.delete)
 
 module.exports = bannerEndpoints
--- server/routes/endpoints/categories.js
+++ server/routes/endpoints/categories.js
@@ -1,11 +1,12 @@
 const categoryEndpoints = require('express').Router()
+const { authenticate, authorize } = require('../../middleware/auth')
 const { CategoryController } = require('../../controllers')
 
 categoryEndpoints
 .get('/', CategoryController.read)
 .get('/:id', CategoryController.readOne)
-.post('/', CategoryController.create)
-.put('/:id', CategoryController.update)
-.delete('/:id', CategoryController.delete)
+.post('/', authenticate, authorize, CategoryController.create)
+.put('/:id', authenticate, authorize, CategoryController.update)
+.delete('/:id', authenticate, authorize, CategoryController.delete)
 
 module.exports = categoryEndpoints
