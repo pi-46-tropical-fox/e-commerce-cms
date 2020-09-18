46d21d5310ccfd88245d159e9c8937b63ef391eb
Eas <me@eas.web.id>
2020-09-19 03:23:14 +0700

    routing fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,13 +8,11 @@ const basename = path.basename(__filename)
 
 fs
     // read the whole file contents inside current dir
-    .readdirSync(__dirname)
+    .readdirSync(`${__dirname}/endpoints`)
     // filters out index.js and other non-JS files
     .filter(file => (file.indexOf !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
     // then adds filtered list to the endpoints object
-    .forEach(file => {
-        base.use(`/${file}`, require(path.join(__dirname, file)))
-    })
+    .forEach(file => base.use(`/${file.split('.')[0]}`, require(path.join(`${__dirname}/endpoints`, file))))
 
 // exports the endpoints
 module.exports = base
