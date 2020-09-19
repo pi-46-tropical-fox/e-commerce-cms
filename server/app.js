8d5f2ecab0d380d97a8c1313f7c3c894167e29d6
Naufal Muhamad Siddiq <obostea12@gmail.com>
2020-09-19 17:45:07 +0700

    gitignore

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const errorHandler = require(`./middlewares/errHandler`)
 const app = express()
 const port = process.env.PORT || 3000
 
-//console.log(process.env.SECRET)
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
