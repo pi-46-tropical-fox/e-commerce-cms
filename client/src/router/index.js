9f105a0be802a1d793f2da03579b4eab3aa30c56
unknown <izra.arzi@gmail.com>
2020-09-19 17:31:34 +0700

    deployed firebase, added guard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,14 +14,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: '/login',
     name: 'Login',
     component: Login
@@ -36,7 +28,14 @@ const routes = [
         name: 'Product Per Category',
         component: ProductCards
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('access_token')) {
+        next()
+      } else {
+        next('/login')
+      }
+    }
   }
 ]
 
