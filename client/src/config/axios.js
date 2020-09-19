fd8bb69de4809b3ba0d2edb79d889cd5740c6888
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-19 17:50:52 +0700

    Setting deploy firebase

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://e-commerce-cms-bdh8.herokuapp.com',
 });
 
 export default instance;
