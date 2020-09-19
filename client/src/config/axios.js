676210036ea36d482bbb223a2dfd065ac059f890
juanbrundism <juanbrundism@gmail.com>
2020-09-19 16:02:17 +0700

    Adding heroku urlbase-II

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 const instance = axios.create({
-  baseURL: 'https://e-commerce-cms-juan.herokuapp.com/'
+  baseURL: 'https://e-commerce-cms-juan.herokuapp.com'
 })
 // const instance = axios.create({
 //   baseURL: 'http://localhost:3001'
