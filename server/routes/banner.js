d8da06834900d4acca1be3f98dbd02ed2d3b593b
ArvianditoCaessara <arviandito.caessara@mail.com>
2020-09-19 16:51:16 +0700

    update4

--- server/routes/banner.js
+++ server/routes/banner.js
@@ -4,7 +4,7 @@ const { authentication, authorizationRole } = require('../middlewares/auth')
 
 bannerRoutes.post('/banners', authentication, authorizationRole, BannerController.addBanner)
 bannerRoutes.get('/banners', authentication, authorizationRole, BannerController.getBanner)
-bannerRoutes.get('/banners/:id', BannerController.getBannerId)
+bannerRoutes.get('/banners/:id', authentication, authorizationRole, BannerController.getBannerId)
 bannerRoutes.put('/banners/:id', authentication, authorizationRole, BannerController.updateBanner)
 bannerRoutes.delete('/banners/:id', authentication, authorizationRole, BannerController.deleteBanner)
 
--- server/routes/product.js
+++ server/routes/product.js
@@ -4,7 +4,7 @@ const { authentication, authorizationRole } = require('../middlewares/auth')
 
 productRoutes.post('/products', authentication, authorizationRole, ProductController.addProduct)
 productRoutes.get('/products', authentication, authorizationRole, ProductController.getProduct)
-productRoutes.get('/products/:id', ProductController.getProductId)
+productRoutes.get('/products/:id', authentication, authorizationRole, ProductController.getProductId)
 productRoutes.put('/products/:id', authentication, authorizationRole, ProductController.updateProduct)
 productRoutes.delete('/products/:id', authentication, authorizationRole, ProductController.deleteProduct)
 
